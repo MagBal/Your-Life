176f092ef90fd0fa973e51e803e950ee4e2d5dea
Magda Balun <magda.balun@gmail.com>
2018-04-18 10:08:17 +0200

    Format files

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 //smooth scrolling navbar
-$("#nav ul li a[href^='#']").on('click', function(e) {
-	// prevent default anchor click behavior
-	e.preventDefault();
-    // animate
-    $('html, body').animate({
-    	scrollTop: $(this.hash).offset().top
-    }, 1000, function(){
-    // when done, add hash to url
-    // (default click behaviour)
-       	window.location.hash = this.hash;
-    });
+$("#nav ul li a[href^='#']").on("click", function(e) {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $(this.hash).offset().top
+    },
+    1000,
+    function() {
+      window.location.hash = this.hash;
+    }
+  );
 });
 
 //multiSlideShow
 
-var slideIndex = [1,1];
+var slideIndex = [1, 1];
 /*Two slideshow group with different classes*/
-var slideId = ["mySlides1", "mySlides2"] 
+var slideId = ["mySlides1", "mySlides2"];
 showSlides(1, 0);
 showSlides(1, 1);
 
 function plusSlides(n, nd) {
-  showSlides(slideIndex[nd] += n, nd);
+  showSlides((slideIndex[nd] += n), nd);
 }
 
 function showSlides(n, nd) {
   var i;
   var x = document.getElementsByClassName(slideId[nd]);
-  if (n > x.length) {slideIndex[nd] = 1} 
-  if (n < 1) {slideIndex[nd] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
+  if (n > x.length) {
+    slideIndex[nd] = 1;
   }
-  x[slideIndex[nd]-1].style.display = "block"; 
+  if (n < 1) {
+    slideIndex[nd] = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[nd] - 1].style.display = "block";
 }
-
